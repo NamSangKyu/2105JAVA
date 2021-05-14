6fba622c9b7bf1338ef9b23cfef6d608b8dee89e
nam2626 <nam2626@gmail.com>
2021-05-14 19:59:45 +0900

    변수 타입 설명

--- Step02/src/PrintValue.java
+++ Step02/src/PrintValue.java
@@ -1,9 +1,39 @@
 
 public class PrintValue {
-
+	/*
+	 * 데이터 타입 - Primitive type(고정형)
+	 * 		정수형 : 양수, 음수, 0 저장하는 데이터 타입
+	 * 		실수형 : 소수점까지 숫자를 저장하는 데이터 타입
+	 * 		문자형 : 글자 한글자를 저장하는 데이터 타입
+	 * 		논리형 : true와 false 둘중하나를 저장하는 데이터 타입
+	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//숫자 출력
+		System.out.println(10);
+		System.out.println("10"+10);//문자열이랑 다른 값이랑 덧셈을 하면 하나의 문자열로 만듬
+		System.out.println(10+10);
+		System.out.println(-777);
+		//실수 출력
+		System.out.println(3.1415);
+		//반지름이 3인 원넓이 출력
+		System.out.println(3*3*3.1415);
+		System.out.println("반지름 3인 원넓이 : "+3*3*3.1415);
+		//문자 출력
+		System.out.println('A');
+		System.out.println('가');
+		//논리형 출력
+		System.out.println(true);
+		System.out.println(false);
+		
+		
 	}
 
 }
+
+
+
+
+
+
+
+
---
+++ Step02/src/VariableEx1.java
@@ -0,0 +1,16 @@
+
+public class VariableEx1 {
+	/*
+	 * Variable(변수) : 데이터를 하나 저장할 공간
+	 * 	변수 타입
+	 * 		정수형 : byte(1) short(2) int(4) long(8)
+	 * 		실수형 : float(4) double(8)
+	 * 		문자형 : char(2)
+	 * 		논리형 : boolean(1)
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
