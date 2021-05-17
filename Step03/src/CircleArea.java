34521cdf3d514106cbc53dfb53ab0f3bb946b591
nam2626 <nam2626@gmail.com>
2021-05-17 19:59:18 +0900

    원넓이 예제

--- Step03/src/CircleArea.java
+++ Step03/src/CircleArea.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class CircleArea {
 	/*
@@ -5,8 +6,19 @@ public class CircleArea {
 	 *  입력 받은 반지름을 이용하여 원넓이 출력 
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		
+		//변수를 선언 - 반지름(int), 원넓이(double)
+		int r;
+		double area;
+		
+		//입력 및 초기화
+		System.out.println("반지름 입력 : ");
+		r = sc.nextInt();
+		//계산
+		area = r*r*3.1415;
+		//결과를 출력
+		System.out.println("반지름 "+r+"인 원의 넓이는 "+area);
 	}
 
 }
