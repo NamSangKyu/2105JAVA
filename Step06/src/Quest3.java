d3ee46226cdf0e2c51e8105ee50d08de33df243c
nam2626 <nam2626@gmail.com>
2021-05-28 21:36:46 +0900

    문제 풀이

--- Step06/src/Quest3.java
+++ Step06/src/Quest3.java
@@ -1,9 +1,30 @@
+import java.util.Scanner;
 
 public class Quest3 {
 
 	public static void main(String[] args) {
 		//숫자 두개를 입력 받은 후
 		//입력 받은 두 수의 최대 공약수를 출력
+		int n1, n2, gcd=1;
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("숫자 입력 : ");
+		n1 = sc.nextInt();
+		System.out.print("숫자 입력 : ");
+		n2 = sc.nextInt();
+		
+		if(n1>n2) {
+			//n1과 n2의 값을 교환
+			int temp = n1;
+			n1 = n2;
+			n2 = temp;
+		}
+				
+		for(int i=2;i<=n1;i++) {
+			if(n1 % i == 0 && n2 % i == 0)
+				gcd = i;
+		}
+		System.out.println("두 수의 최대 공약수 : " + gcd);
 	}
 
 }
---
+++ Step06/src/Quest4.java
@@ -0,0 +1,10 @@
+
+public class Quest4 {
+
+	public static void main(String[] args) {
+		//숫자 하나를 입력 받은 후
+		//입력 숫자가 소수인지 아닌지 판단하는 코드를 작성
+		//소수는 1과 자기자신 숫자로만 나누어지는 숫자
+	}	
+
+}
