c933d0096cd929c7228e08f81bf25a6ccd3412bc
nam2626 <nam2626@gmail.com>
2021-06-02 21:48:08 +0900

    setter/getter

--- Step08/src/NameCard.java
+++ Step08/src/NameCard.java
@@ -18,54 +18,91 @@ public class NameCard {
 		this.address = address;
 	}
 
-	//setter/getter 생성
+	/**
+	 * @return the name
+	 */
 	public String getName() {
 		return name;
 	}
 
+	/**
+	 * @param name the name to set
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
 
+	/**
+	 * @return the company
+	 */
 	public String getCompany() {
 		return company;
 	}
 
+	/**
+	 * @param company the company to set
+	 */
 	public void setCompany(String company) {
 		this.company = company;
 	}
 
+	/**
+	 * @return the tel
+	 */
 	public String getTel() {
 		return tel;
 	}
 
+	/**
+	 * @param tel the tel to set
+	 */
 	public void setTel(String tel) {
 		this.tel = tel;
 	}
 
+	/**
+	 * @return the email
+	 */
 	public String getEmail() {
 		return email;
 	}
 
+	/**
+	 * @param email the email to set
+	 */
 	public void setEmail(String email) {
 		this.email = email;
 	}
 
+	/**
+	 * @return the position
+	 */
 	public String getPosition() {
 		return position;
 	}
 
+	/**
+	 * @param position the position to set
+	 */
 	public void setPosition(String position) {
 		this.position = position;
 	}
 
+	/**
+	 * @return the address
+	 */
 	public String getAddress() {
 		return address;
 	}
 
+	/**
+	 * @param address the address to set
+	 */
 	public void setAddress(String address) {
 		this.address = address;
 	}
+
+	//setter/getter 생성
 	
 	
 }
