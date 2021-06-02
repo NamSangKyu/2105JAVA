6fa11228f627aa81871e747a671dd17fd09348e9
nam2626 <nam2626@gmail.com>
2021-06-02 21:34:03 +0900

    setter/getter 적용

--- Step08/src/Student.java
+++ Step08/src/Student.java
@@ -22,7 +22,38 @@ public class Student {
 		this.major = major;
 		this.score = score;
 	}
-
+	//학번 정보를 외부로 반출
+	public String getSno() {
+		return sno;
+	}
+	//학번 정보를 외부로부터 받아서 최신화
+	public void setSno(String sno) {
+		this.sno = sno;
+	}
+	
+	
+	//이름 set/get
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	//학과 set/get
+	public String getMajor() {
+		return major;
+	}
+	public void setMajor(String major) {
+		this.major = major;
+	}
+	//평점 set/get --> 0.0 이상 4.5이하만 저장
+	public double getScore() {
+		return score;
+	}
+	public void setScore(double score) {
+		if(score < 0.0 || score > 4.5) return;
+		this.score = score;
+	}
 	// 성적 등급을 구하는 메서드
 	public String getGrade() {
 		String grade = "F";
