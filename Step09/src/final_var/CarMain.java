c996efadde8d3320b9017da67f1c530f0c266a68
nam2626 <nam2626@gmail.com>
2021-06-07 19:53:19 +0900

    Car main class

---
+++ Step09/src/final_var/CarMain.java
@@ -0,0 +1,13 @@
+package final_var;
+
+public class CarMain {
+
+	public static void main(String[] args) {
+		Car k3 = new Car(240);
+		Car k7 = new Car(300);
+		
+		k3.carInfo();
+		k7.carInfo();
+	}
+
+}
