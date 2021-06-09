2e0bbe7614a83feb85d186b00051eb10383aaa36
nam2626 <nam2626@gmail.com>
2021-06-09 21:13:38 +0900

    삭제 작업 완료

--- MemberProject/src/MemberService.java
+++ MemberProject/src/MemberService.java
@@ -68,7 +68,25 @@ public class MemberService {
 		System.out.println("회원정보 수정이 완료되었습니다.");
 	}
 	//삭제
-	
+	public void deleteMemberVO(Scanner sc) {
+		//삭제할 데이터를 검색
+		String name;
+		System.out.println("회원 정보 삭제를 시작합니다.........");
+		System.out.println("삭제할 회원명 : ");
+		name = sc.nextLine();
+		
+		for (int i = 0; i < index; i++) {
+			if(arr[i].getName().equals(name)) {
+				for(int j=i;j<index-1;j++) {
+					arr[j] = arr[j+1];
+				}
+				index--;
+				System.out.println("삭제가 완료 되었습니다.");
+				return;
+			}
+		}
+		System.out.println("삭제할 데이터가 없습니다.");
+	}
 	
 	
 	
